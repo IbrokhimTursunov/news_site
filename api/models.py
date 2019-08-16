1602ca14b773cec589d4e032fdb4b3e538b61430
Ibrokhim Tursunov <ibragim1989@gmail.com>
2019-08-16 22:00:08 +0300

    Add serializer for News.

--- api/models.py
+++ api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(default='', blank=True, primary_key=False)
+    title = models.CharField(default='', blank=True, primary_key=False, max_length=2048)
     content = models.TextField(default='', blank=True, primary_key=False)
